43ba3276b62d98a47077d8310a2a35a0325c1b8d
Benjamin Van Renterghem <benjamin.v.renterghem@gmail.com>
2020-01-23 22:10:42 +0100

    Basic React app setup

---
+++ frontend/src/App/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+/**
+ * TODO:
+ *  - Manage background music, image ...
+ *  - Show global UI elements (mute button)
+ *  - Manage different screens (main menu, game, leaderboard, help ...)
+ *  - Register modals system (for e.g. pause menu, game over & submit to leaderboard ...)
+ *  - Splash screen (progress for loading all assets, should integrate with the game!)
+ */
+const App: React.FC = () => {
+  return (
+    <h1>Hello, World!</h1>
+  );
+};
+
+export default App;
