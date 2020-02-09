f983a5dd6d515a70be545447be5b988373b7aedf
Benjamin Van Renterghem <benjamin.v.renterghem@gmail.com>
2020-02-09 19:34:40 +0100

    Add Phaser scaling

--- frontend/src/game/FoodSwallowerGame.ts
+++ frontend/src/game/FoodSwallowerGame.ts
@@ -8,6 +8,10 @@ export class FoodSwallowerGame extends Phaser.Game {
       canvas: canvas,
       width: 400,
       height: 400,
+      scale: {
+        mode: Phaser.Scale.ScaleModes.RESIZE,
+        autoCenter: Phaser.Scale.Center.CENTER_BOTH
+      },
       autoFocus: true,
       scene: [
         LoadingScene
